9b6b4105debb60beffb278a5362c604cab122a2e
urnm1 <54842038+urnm1@users.noreply.github.com>
2019-09-28 15:49:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,4 @@
-#include <gsl/gsl>
-
-void freemem(gsl::owner<int*> var);
-
 int main(int argc, char* argv[])
 {
-  auto *i = new int{3};
   return 0;
-}
-
-void freemem(gsl::owner<int*> var)
-{
-  delete var;
 }
